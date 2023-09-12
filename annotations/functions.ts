a825ec93e7f3b9cf95e91cdb423e84703c6b6212
Burma Jason <sawyehtet845@gmail.com>
2023-09-12 12:29:59 +0630

    destructuring with annotations

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -21,3 +21,21 @@ const logger = (message : string) : void => {
 const throwError =  (message : string) : never => {
     throw new Error(message);
 }
+
+
+const forecast = {
+    date : new Date(),
+    weather : 'sunny'
+};
+
+// const logWeather = (forecast : { date : Date, weather : string }) : void => {
+//     console.log(`Date: ${forecast.date}, Weather: ${forecast.weather}`);
+// }
+
+// es2015
+const logWeather = ({date,weather} : { date:Date; weather: string }) : void => {
+    console.log(date);
+    console.log(weather);
+}
+
+logWeather(forecast);
